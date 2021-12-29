817c3c87bf90563f16e0d500eecd576b3c120a4a
ilgolf <ilgolc@naver.com>
2021-12-29 15:37:50 +0900

    "initial commit"

---
+++ src/test/java/me/golf/real/jpa/RealJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.golf.real.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
